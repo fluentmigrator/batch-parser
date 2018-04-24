46526abf843d37ea176837b935c77dd55062a183
Mark Junker <m.junker@fubar-dev.de>
2018-04-24 21:20:23 +0200

    Unit test for fluentmigrator/fluentmigrator#859

--- FluentMigrator.BatchParser.Tests/RangeSearcherTests.cs
+++ FluentMigrator.BatchParser.Tests/RangeSearcherTests.cs
@@ -301,6 +301,7 @@ namespace FluentMigrator.BatchParser.Tests
 
         [TestCase("   # qweqwe", " qweqwe", "   ")]
         [TestCase("   # qwe\nqwe", " qwe", "   \nqwe")]
+        [TestCase("asd # qwe\nqwe", "", "asd # qwe\nqwe")]
         public void TestPoundSignSingleLineComment(string input, string expectedComment, string expectedOther)
         {
             var source = new TextReaderSource(new StringReader(input));
